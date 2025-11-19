2a174aaf44f815eab781edba417cd543c2e5ddb3
KenzyHaruto <kritoexe39@gmail.com>
2025-11-19 19:21:25 +0700

    uts.py

--- uts.py
+++ uts.py
@@ -28,6 +28,20 @@ def convertWaktu(jenis, waktu):
     else:
         print(f"{jenis}: {waktu}menit")
 
+def tampilKegiatan():
+    # Tampilkan daftar
+    os.system("cls")
+    print("Daftar kegiatan: ")
+    for a in range(len(daftar_kegiatan)):
+        print(f"{a + 1}. {daftar_kegiatan[a][0]}({daftar_kegiatan[a][1]} menit)")
+
+    # Tampilkan terpilih
+    print(f"\nWaktu luang: {waktu_luang} menit\nKegiatan terpilih:")
+    for a in range(len(kegiatan_terpilih)):
+        print(f"{a + 1}. {kegiatan_terpilih[a][0]}({kegiatan_terpilih[a][1]} menit)")
+    convertWaktu("Total waktu", total_waktu)
+    convertWaktu("Sisa waktu", waktu_luang - total_waktu)
+
 # Waktu luang
 waktu_luang = cekInputWaktu("luang")
 
@@ -49,23 +63,14 @@ while True:
     if input("Masukkan 'y' untuk input lagi: ") != "y":
         break
 
-# Tampilkan daftar
-os.system("cls")
-print("Daftar kegiatan: ")
-for a in range(len(daftar_kegiatan)):
-    print(f"{a + 1}. {daftar_kegiatan[a][0]}({daftar_kegiatan[a][1]} menit)")
-
 # Cari dan tampilkan
-print(f"\nWaktu luang: {waktu_luang} menit\nKegiatan terpilih:")
 for a in daftar_kegiatan:
     if waktu_luang >= (total_waktu + a[1]):
         kegiatan_terpilih.append(a)
         total_waktu += a[1]
-        print(f"{len(kegiatan_terpilih)}. {a[0]}({a[1]} menit)")
         if waktu_luang == total_waktu:
             break
-convertWaktu("Total waktu", total_waktu)
-convertWaktu("Sisa waktu", waktu_luang - total_waktu)
+tampilKegiatan() 
 
 # Cari kegiatan yang tidak masuk terpilih
 salinan_terpilih = [a[0] for a in kegiatan_terpilih]
@@ -74,7 +79,7 @@ for a in daftar_kegiatan:
         kegiatan_tdkterpilih.append(a)
 
 # Jika ada kegiatan tidak terpilih
-if len(kegiatan_tdkterpilih) >= 1:
+if len(kegiatan_tdkterpilih) > 0:
     # Tampilkan kegiatan tidak terpilih dan input tambahan waktu luang
     print("\nKegiatan tidak terpilih:")
     for a in range(len(kegiatan_tdkterpilih)):
@@ -98,28 +103,20 @@ if len(kegiatan_tdkterpilih) >= 1:
 
 # Jika ada waktu sisa
 elif (waktu_luang - total_waktu) > 0:
-    #Keluar program jika tidak ingin input kegiatan lagi
-    if input("Masih ada sisa waktu, ingin input kegiatan lagi?(y/t): ") != "y":
-        exit()
-    
     # Input kegiatan dan cek dan masukkan
-    kegiatan = [input("Masukkan nama kegiatan: "), cekInputWaktu("kegiatan")]
-    if kegiatan[1] > (waktu_luang - total_waktu):
-        print("Waktu luang tidak cukup, tidak bisa menambahkan kegiatan")
-        exit()
-    daftar_kegiatan.append(kegiatan)
-    kegiatan_terpilih.append(kegiatan)
-    total_waktu += kegiatan[1]
-
-# #Tampilkan daftar
-os.system("cls")
-print("Diperbarui!!\nDaftar kegiatan: ")
-for a in range(len(daftar_kegiatan)):
-    print(f"{a + 1}. {daftar_kegiatan[a][0]}({daftar_kegiatan[a][1]} menit)")
-
-# Tampilkan kegiatan terpilih
-print(f"\nWaktu luang: {waktu_luang} menit\nKegiatan terpilih:")
-for a in range(len(kegiatan_terpilih)):
-    print(f"{a + 1}. {kegiatan_terpilih[a][0]}({kegiatan_terpilih[a][1]} menit)")
-convertWaktu("Total waktu", total_waktu)
-convertWaktu("Sisa waktu", waktu_luang - total_waktu)
+    print("Masih ada sisa waktu, inputkan kegiatan lagi")
+    salinan_daftar = [a[0].lower() for a in daftar_kegiatan]
+    while True:
+        nama_kegiatan = input("Masukkan nama kegiatan: ")
+        if nama_kegiatan.lower() not in salinan_daftar: 
+            waktu_kegiatan = cekInputWaktu("kegiatan")
+            if waktu_kegiatan > (waktu_luang - total_waktu):
+                print("Waktu luang tidak cukup, tidak bisa menambahkan kegiatan")
+                exit()
+            daftar_kegiatan.append([nama_kegiatan, waktu_kegiatan])
+            kegiatan_terpilih.append([nama_kegiatan, waktu_kegiatan])
+            total_waktu += waktu_kegiatan
+            break
+        else:
+            print("Nama kegiatan sudah ada")
+tampilKegiatan()
